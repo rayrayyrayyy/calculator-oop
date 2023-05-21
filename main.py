8fac4d53cb8963633d6bb7432f6f3f0de11bf786
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-21 08:52:19 +0800

    import class and tkinter

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # import class
+from calculator import CalculatorOop
+
+# import tkinter library
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
 # call methods
 # display output
+
 # end program
-# {+++ design}
