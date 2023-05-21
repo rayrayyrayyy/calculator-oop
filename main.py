61ae4d81e9611296de33d3d9ffe8984e75fa344b
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-21 08:54:51 +0800

    create tkinter window

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
+# open a window using tkinter
+root_window = Tk()
+root_window.title("OOP CALCULATOR")
+root_window.geometry("400x400")
+root_window.config(bg = "yellow")
+
 # call methods
 # display output
 
+mainloop()
 # end program
