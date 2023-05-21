14fa956e1d8936eaef73c63785d186ead0f9568f
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-21 09:08:32 +0800

    label for second number

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ root_window.title("OOP CALCULATOR")
 root_window.geometry("400x400")
 root_window.config(bg = "yellow")
 
-# create buttons and labels
+# create entry box and labels
 note_1 = Label(root_window, text = "Enter first number:", bg = "yellow", font = ("Times", 15), justify = CENTER) # label for first num
 note_1.place(x=10, y=10)
 number_1 = Entry(root_window, font=("Segoe Script",10), justify = CENTER) # entry box for first num
 number_1.place(x=170, y=10)
+
+note_2 = Label(root_window, text = "Enter second number:", bg = "yellow", font = ("Times", 15), justify = CENTER) # label for second num
+note_2.place(x=10, y=40)
 
 # call methods
 # display output
