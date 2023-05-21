bb15c97b16dd7334e46cc312c7871fdca9422fb9
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-21 09:55:30 +0800

    trial

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # import class
 from calculator import CalculatorOop
 
+# call methods
+def main():
+    compute = CalculatorOop()
+    compute.addition(number_1, number_2)
+
+
 # import tkinter library
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-
 # open a window using tkinter
 root_window = Tk()
 root_window.title("OOP CALCULATOR")
@@ -23,7 +28,11 @@ note_2.place(x=10, y=40)
 number_2 = Entry(root_window, font=("Segoe Script",10), justify = CENTER) # entry box for second num
 number_2.place(x=190, y=40)
 
-# call methods
+#create buttons
+# addition button
+add_button = Button(root_window, text = "ADD", width = 20, command = main)
+add_button.place(x=30, y=90)
+
 # display output
 
 mainloop()
