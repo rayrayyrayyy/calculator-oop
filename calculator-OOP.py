1f8939482b6b3852c864a6fb14d3940e8673c4a7
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-21 08:30:36 +0800

    create instance variables

--- calculator-OOP.py
+++ calculator-OOP.py
@@ -4,6 +4,8 @@
 class CalculatorOop:
     def __init__(self):
     # create instance variables
+        self.first_num = float(input("Enter first number: "))
+        self.second_num = float(input("Enter first num: "))
 # define addition function
 # define subtraction function
 # define multiplication function
