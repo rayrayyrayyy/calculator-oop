c104ddf9affee7dd572307b8c369d5d1352968ff
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-23 00:52:53 +0800

    remove tkinter

--- calculator.py
+++ calculator.py
@@ -3,9 +3,7 @@
 # create class for calculator
 class CalculatorOop:
     def __init__(self):
-    # create instance variables
-        self.first_num = 0
-        self.second_num = 0
+        pass
     
     # define addition function
     def addition(self):
--- main.py
+++ main.py
@@ -2,38 +2,7 @@
 from calculator import CalculatorOop
 
 # call methods
-def main():
-    compute = CalculatorOop()
-    compute.addition(number_1 + number_2)
+compute = CalculatorOop()
 
-
-# import tkinter library
-from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
-# open a window using tkinter
-root_window = Tk()
-root_window.title("OOP CALCULATOR")
-root_window.geometry("400x400+800+350")
-root_window.config(bg = "yellow")
-
-# create entry box and labels
-note_1 = Label(root_window, text = "Enter first number:", bg = "yellow", font = ("Times", 15), justify = CENTER) # label for first num
-note_1.place(x=10, y=10)
-number_1 = Entry(root_window, font=("Segoe Script",10), justify = CENTER) # entry box for first num
-number_1.place(x=170, y=10)
-
-note_2 = Label(root_window, text = "Enter second number:", bg = "yellow", font = ("Times", 15), justify = CENTER) # label for second num
-note_2.place(x=10, y=40)
-number_2 = Entry(root_window, font=("Segoe Script",10), justify = CENTER) # entry box for second num
-number_2.place(x=190, y=40)
-
-#create buttons
-# addition button
-add_button = Button(root_window, text = "ADD", width = 20, command = main)
-add_button.place(x=30, y=90)
-
-# display output
-
-mainloop()
+compute.__init__()
 # end program
